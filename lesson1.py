516f8163c5dadb783d922074e796885ced01601b
Alexbekfronspb <108635395+Alexbekfronspb@users.noreply.github.com>
2022-11-02 16:55:50 +0300

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+hello = "hello world!"
+print(hello)
+name = "Alexander"
+surname = "Bekuzarov"
+print(name + " " + surname, end = "")# конкатенация - сложение строк
+print(name * 13)# репликация - умножение числа на строку
+_ = 13
+print(_, _, sep = "*")
+# my_age = input("1")
+# print(my_age)
+len_name = len(name)
+print(len_name)
+number = 13
+number = "13"
+text = "мое любимое число"
+print(text + str(number))
+print(text + f"{number}") # тайпкастинг - конвертирование типов данных
+# str(), int(), float()
+print(float(_))
+print(10 == "10")
+print(10 == 0010.0)
+# bool, int, float, complex, str, list, tuple, bytes, bytearray, set, frozenset, dict
+# Name variable a-z, A-Z, 0-9,_, sensitive to register, not begin from numbers
+finally1 = test = 1
+print(type(test))
+
+
+# написать калькулятор, повторить весь материал, проработать сложение строк
